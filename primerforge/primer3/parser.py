0586c916468602502c2ebaf8c9b6d23c1187c9f4
Abhishek Warad <ishek619@gmail.com>
2026-07-11 11:54:04 +0530

    Make Primer3 parser coordinate-native

--- primerforge/primer3/parser.py
+++ primerforge/primer3/parser.py
@@ -2,13 +2,20 @@
 Primer3 result parser.
 """
 
-from primerforge.models.primer import Primer
 from primerforge.models.pair import PrimerPair
+from primerforge.models.primer import Primer
+from primerforge.reference.coordinates import Coordinate
 
 
 class Primer3Parser:
+    """
+    Parse Primer3 output into PrimerForge models.
+    """
 
-    def parse_left(self, result):
+    def parse_left(
+        self,
+        result,
+    ):
 
         primers = []
 
@@ -23,29 +30,54 @@ class Primer3Parser:
                 f"PRIMER_LEFT_{i}"
             ]
 
+            start += 1
+
+            end = start + length - 1
+
             primers.append(
 
                 Primer(
+
                     sequence=result[
                         f"PRIMER_LEFT_{i}_SEQUENCE"
                     ],
-                    start=start + 1,
-                    end=start + length,
+
+                    start=start,
+
+                    end=end,
+
                     strand="+",
+
                     length=length,
+
                     tm=result[
                         f"PRIMER_LEFT_{i}_TM"
                     ],
+
                     gc=result[
                         f"PRIMER_LEFT_{i}_GC_PERCENT"
                     ],
+
+                    coordinate=Coordinate(
+
+                        start=start,
+
+                        end=end,
+
+                        system="reference",
+
+                    ),
+
                 )
 
             )
 
         return primers
 
-    def parse_right(self, result):
+    def parse_right(
+        self,
+        result,
+    ):
 
         primers = []
 
@@ -60,33 +92,62 @@ class Primer3Parser:
                 f"PRIMER_RIGHT_{i}"
             ]
 
+            start += 1
+
+            end = start + length - 1
+
             primers.append(
 
                 Primer(
+
                     sequence=result[
                         f"PRIMER_RIGHT_{i}_SEQUENCE"
                     ],
-                    start=start + 1,
-                    end=start + length,
+
+                    start=start,
+
+                    end=end,
+
                     strand="-",
+
                     length=length,
+
                     tm=result[
                         f"PRIMER_RIGHT_{i}_TM"
                     ],
+
                     gc=result[
                         f"PRIMER_RIGHT_{i}_GC_PERCENT"
                     ],
+
+                    coordinate=Coordinate(
+
+                        start=start,
+
+                        end=end,
+
+                        system="reference",
+
+                    ),
+
                 )
 
             )
 
         return primers
 
-    def parse_pairs(self, result):
+    def parse_pairs(
+        self,
+        result,
+    ):
 
-        forward = self.parse_left(result)
+        forward = self.parse_left(
+            result,
+        )
 
-        reverse = self.parse_right(result)
+        reverse = self.parse_right(
+            result,
+        )
 
         pairs = []
 
@@ -98,29 +159,40 @@ class Primer3Parser:
         for i in range(n):
 
             product = result.get(
+
                 f"PRIMER_PAIR_{i}_PRODUCT_SIZE",
-                reverse[i].end - forward[i].start + 1,
+
+                reverse[i].coordinate.end
+                - forward[i].coordinate.start
+                + 1,
+
             )
 
             pair = PrimerPair(
+
                 forward=forward[i],
+
                 reverse=reverse[i],
+
                 product_size=product,
+
             )
 
-            if (
-                f"PRIMER_PAIR_{i}_PENALTY"
-                in result
-            ):
+            penalty = result.get(
+                f"PRIMER_PAIR_{i}_PENALTY",
+            )
+
+            if penalty is not None:
+
                 pair.breakdown[
                     "primer3_penalty"
                 ] = round(
-                    result[
-                        f"PRIMER_PAIR_{i}_PENALTY"
-                    ],
+                    penalty,
                     3,
                 )
 
-            pairs.append(pair)
+            pairs.append(
+                pair,
+            )
 
         return pairs
